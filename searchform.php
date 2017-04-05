aec9a57aa057d2ba12d9cede4420a57aa2dede57
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-05 15:38:26 -0500

    init

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <label class="u-visuallyhidden" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+  <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+  <input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+</form>
