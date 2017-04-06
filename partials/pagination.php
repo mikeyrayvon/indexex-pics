f9fc6f38f424ed452dc59dc9df2360752a52831e
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-06 16:46:58 -0500

    add pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -1,22 +1,24 @@
-<?php
-if( get_next_posts_link() || get_previous_posts_link() ) {
-?>
-  <!-- post pagination -->
-  <nav id="pagination">
-<?php
-$previous = get_previous_posts_link('Newer');
-$next = get_next_posts_link('Older');
-if ($previous) {
-  echo $previous;
-}
-if ($previous && $next) {
-  echo ' &mdash; ';
-}
-if ($next) {
-  echo $next;
-}
-?>
-  </nav>
-<?php
-}
-?>
+<nav id="pagination" class="container grid-row">
+  <div class="grid-item item-s-12 text-align-center">
+    <?php
+    global $wp_query;
+
+    $currentLang = qtranxf_getLanguage();
+    $prev_text = $currentLang == 'en' ? 'Newer' : 'Adelante';
+    $next_text = $currentLang == 'en' ? 'Older' : 'Atrás';
+
+    $big = 999999999; // need an unlikely integer
+    $translated = __( 'Page', 'mytextdomain' ); // Supply translatable string
+
+    echo paginate_links( array(
+    	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+    	'format' => '?paged=%#%',
+      'prev_text' => $prev_text,
+      'next_text' => $next_text,
+    	'current' => max( 1, get_query_var('paged') ),
+    	'total' => $wp_query->max_num_pages,
+            'before_page_number' => '<span class="u-visuallyhidden">'.$translated.' </span>'
+    ) );
+    ?>
+  </div>
+</nav>
