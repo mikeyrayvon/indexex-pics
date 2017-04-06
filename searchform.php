585184fed9a17b3e924b2794a0d7e8934405fc9a
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-06 16:46:30 -0500

    add custom searchform

--- searchform.php
+++ searchform.php
@@ -1,5 +1,9 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" id="searchform" class="searchform grid-row" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <label class="u-visuallyhidden" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-  <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-  <input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+  <div class="grid-item item-s-8">
+    <input id="search-text-input" type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+  </div>
+  <div class="grid-item item-s-4 text-align-right">
+    <input type="submit" id="searchsubmit" value="<?php _e('[:es]Buscar[:en]Search[:]'); ?>" />
+  </div>
 </form>
