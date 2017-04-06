cbf941d3bdc67d2ccf856182e00b51da599030f0
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-06 16:46:49 -0500

    add metaboxes

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -39,5 +39,84 @@ function igv_cmb_metaboxes() {
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
 	 */
 
+  $expo_meta = new_cmb2_box( array(
+    'id'            => $prefix . 'expo',
+    'title'         => esc_html__( 'Expo Meta', 'cmb2' ),
+    'object_types'  => array( 'post', ),
+ 	) );
+
+  $expo_meta->add_field( array(
+		'name' => esc_html__( 'Open Date', 'cmb2' ),
+		'id'   => $prefix . 'expo_date_open',
+		'type' => 'text_date_timestamp',
+	) );
+
+  $expo_meta->add_field( array(
+		'name' => esc_html__( 'Close Date', 'cmb2' ),
+		'id'   => $prefix . 'expo_date_close',
+		'type' => 'text_date_timestamp',
+	) );
+
+  $expo_meta->add_field( array(
+		'name'    => esc_html__( 'Location', 'cmb2' ),
+		'id'      => $prefix . 'expo_location',
+		'type'    => 'wysiwyg',
+		'options' => array( 'textarea_rows' => 4, ),
+	) );
+
+  $expo_meta->add_field( array(
+		'name' => esc_html__( 'Map Embed', 'cmb2' ),
+		'id'   => $prefix . 'expo_map',
+		'type' => 'textarea_code',
+	) );
+
+  $expo_meta->add_field( array(
+		'name'    => esc_html__( 'Documentation', 'cmb2' ),
+		'id'      => $prefix . 'expo_docu',
+		'type'    => 'wysiwyg',
+		'options' => array( 'textarea_rows' => 10, ),
+	) );
+
+  $expo_front_group = $expo_meta->add_field( array(
+		'id'          => $prefix . 'expo_front',
+    'name'    => esc_html__( 'Front images', 'cmb2' ),
+		'type'        => 'group',
+		'options'     => array(
+			'group_title'   => esc_html__( 'Image {#}', 'cmb2' ), // {#} gets replaced by row number
+			'add_button'    => esc_html__( 'Add Another Image', 'cmb2' ),
+			'remove_button' => esc_html__( 'Remove Image', 'cmb2' ),
+			'sortable'      => true, // beta
+			'closed'     => true, // true to have the groups closed by default
+		),
+	) );
+
+  $expo_meta->add_group_field( $expo_front_group, array(
+		'name' => esc_html__( 'Image', 'cmb2' ),
+		'id'   => 'image',
+		'type' => 'file',
+	) );
+
+  // INFO
+
+  $info_meta = new_cmb2_box( array(
+    'id'            => $prefix . 'info_meta',
+    'title'         => esc_html__( 'Info Meta', 'cmb2' ),
+    'object_types'  => array( 'page', ),
+ 	) );
+
+  $info_meta->add_field( array(
+		'name'    => esc_html__( 'Donate', 'cmb2' ),
+		'id'      => $prefix . 'info_donate',
+		'type'    => 'wysiwyg',
+		'options' => array( 'textarea_rows' => 5, ),
+	) );
+
+  $info_meta->add_field( array(
+		'name'    => esc_html__( 'Submit', 'cmb2' ),
+		'id'      => $prefix . 'info_submit',
+		'type'    => 'wysiwyg',
+		'options' => array( 'textarea_rows' => 10, ),
+	) );
+
 }
 ?>
