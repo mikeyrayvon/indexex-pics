f41ad0f598f8c7a3553f003ec002daa8e0b55be0
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-08 20:40:42 -0500

    scaffold 404

--- 404.php
+++ 404.php
@@ -2,9 +2,13 @@
 get_header();
 ?>
 
-<section id="main-content">
-  <h1>Not found</h1>
-  <p>:(</p>
+<section id="main-content" class="container">
+  <div class="grid-row">
+    <div class="grid-item item-s-12 item-m-6">
+      <h1 class="font-size-large">404</h1>
+      <p><?php _e('[:es]No tengo lo que buscas...[:en]I don\'t have what you\'re looking for...[:]'); ?></p>
+    </div>
+  </div>
 </section>
 
 <?php
